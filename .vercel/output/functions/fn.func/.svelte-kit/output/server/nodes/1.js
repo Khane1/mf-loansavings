908d00ff303c1d966fc15669b1263cee83f5c5ba
Khane1 <33218241+Khane1@users.noreply.github.com>
2023-04-13 14:27:48 +0300

    vercel

---
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
+export const file = '_app/immutable/entry/error.svelte.2dd29461.js';
+export const imports = ["_app/immutable/entry/error.svelte.2dd29461.js","_app/immutable/chunks/index.b2a16be1.js","_app/immutable/chunks/singletons.ed551491.js","_app/immutable/chunks/index.e1aac948.js"];
+export const stylesheets = [];
+export const fonts = [];
---
+++ .vercel/output/static/_app/immutable/chunks/0.64a9c7d9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/layout.svelte.40bc7dba.js";export{t as component};
